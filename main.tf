efb54140d1101eaa1a45b2413694ebf60cd10731
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-01-24 16:11:13 -0500

    create ubuntu b&h

---
+++ main.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "b-h" {
+  ami           = data.aws_ami.latest-ubuntu.id
+  instance_type = var.ubuntu_instance_type
+
+  tags = {
+    Name = "dev"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "ubuntu_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+

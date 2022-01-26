848970a0b5d00c991030baa685d3300ca709ca1c
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-01-26 14:14:34 -0500

    add user_data to main.tf and add user_add.yml

--- main.tf
+++ main.tf
@@ -31,6 +31,43 @@ resource "aws_instance" "b-h" {
   key_name               = aws_key_pair.my_key.key_name
   instance_type          = var.ubuntu_instance_type
   vpc_security_group_ids = [aws_security_group.bastion-sg.id]
+  user_data              = <<EOF
+#/bin/bash -x
+yum update -y && yum install ansible -y && mkdir /etc/ansible/roles/user_add/tasks -p
+
+echo "# For when variables are being set from vars_prompt
+- name: Generate users list
+set_fact:
+users:
+- username: '{{ username }}'
+comment: '{{ comment }}'
+group: '{{ group }}'
+addgroups: '{{ addgroups }}'
+pwdhash: '{{ pwdhash }}'
+issudo: '{{ issudo }}'
+when: users is undefined
+
+- name: Add user(s)
+user:
+name: '{{ item.username }}'
+comment: '{{ item.comment }}'
+group: '{{ item.group|default(omit) or omit }}'
+groups: '{{ item.addgroups }}'
+append: yes
+state: present
+password: '{{ item.pwdhash }}'
+update_password: on_create
+with_items: '{{ users }}'
+
+- name: Add user(s) to sudoers
+lineinfile:
+dest: /etc/sudoers
+regexp: '^{{ item.username }}\s'
+line: '{{ item.username }} ALL=(ALL) ALL'
+validate: '/usr/sbin/visudo -cf %s'
+when: item.issudo | lower == 'y'
+with_items: ' {{ users }}'" >> /etc/ansible/roles/user_add/tasks/main.tf
+EOF
 
   tags = {
     Name = "Bastion_Host"
@@ -59,10 +96,10 @@ resource "null_resource" "connect" {
   depends_on = [aws_instance.b-h]
 
   // provisioner "local-exec" {
-  //   command = "ansible-playbook -i inventory user_add.yml"
+  //   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' apache-install.yml"
   // }
 
   provisioner "local-exec" {
     command = "echo 'test2' >> /tmp/txt"
-  }  
+  }
 }

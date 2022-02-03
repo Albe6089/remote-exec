bd25709fc2f8150faedaaf0efada4ecb69e72238
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-03 15:21:16 -0500

    remote backend still not working

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.region
   // required_version = ">=0.14.9" 
 
-   backend "s3" {
-       bucket = "bastion-tfstate-bucket"
-  //      key    = "[Remote_State_S3_Bucket_Key]"
-  //  region = "west-us-2"
-   }
+  backend "s3" {
+    bucket = "bastion-tfstate-bucket"
+    //      key    = "[Remote_State_S3_Bucket_Key]"
+    //  region = "west-us-2"
+  }
 }

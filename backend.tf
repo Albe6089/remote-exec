db03b63134f894b67b8ac5ee6da3018a578c2953
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-03 15:30:49 -0500

    remote backend still not working

--- backend.tf
+++ backend.tf
@@ -2,12 +2,9 @@
 //     bucket = "bastion-tfstate-bucket"
 //   }
 
-
-data "terraform_remote_state" "bastion" {
   backend = "s3"
   config = {
     bucket = var.bucket
     key    = "env:/common/bastion.tfstate"
     region = var.region
   }
-}
